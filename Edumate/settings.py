b7956bb9af6db75a1717d87618f9d024a0a792f3
hussein-hub <hussein.motiwala19@gmail.com>
2022-10-18 18:37:56 +0530

    home page template

--- Edumate/settings.py
+++ Edumate/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Edumate_app.apps.EdumateAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Edumate_app/views.py
+++ Edumate_app/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("HI")
+    return render(request, 'Edumate_app/index.html')
