d329c9be4a44c351498f8676a0009891ec623dfa
hussein-hub <hussein.motiwala19@gmail.com>
2022-10-18 19:54:37 +0530

    some changes

--- Edumate/settings.py
+++ Edumate/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,9 +76,17 @@ WSGI_APPLICATION = 'Edumate.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'edumate_db',
+        'USER': 'root',
+        'PASSWORD': 'hussein',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
