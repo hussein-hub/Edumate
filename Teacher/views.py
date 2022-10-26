bd3f960efd18179fb6ddc244a423f6106fa4021d
hussein-hub <hussein.motiwala19@gmail.com>
2022-10-26 15:30:06 +0530

    minor changes

--- Teacher/views.py
+++ Teacher/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from Edumate_app.models import Teachers
+
 # Create your views here.
 
 def teach_home(request, pk):
-    return render(request, 'Teacher/teacher_home.html')
+    teacher_c = Teachers.objects.get(teach_id=pk)
+    return render(request, 'Teacher/teacher_home.html', {'teacher_c': teacher_c})
