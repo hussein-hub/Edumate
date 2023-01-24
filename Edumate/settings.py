12ada26408155eed3d6903c2eff13cea1bc03eca
Rushabh Gandhi <rushabh.dg@somaiya.edu>
2023-01-24 14:31:14 +0530

    db changed to sqlite

--- Edumate/settings.py
+++ Edumate/settings.py
@@ -79,18 +79,18 @@ WSGI_APPLICATION = 'Edumate.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'edumate_db',
-        'USER': 'root',
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'edumate_db',
+    #     'USER': 'root',
+    #     'PASSWORD': config('DATABASE_PASSWORD'),
+    #     'HOST':'localhost',
+    #     'PORT':'3306',
+    # }
 }
 
 
